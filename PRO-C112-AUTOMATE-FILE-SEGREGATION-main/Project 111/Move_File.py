5d3f0cc06dba19cbbb369516990d20fbfec62734
SajedMajed <103050447+SajedMajed@users.noreply.github.com>
2023-09-04 16:37:57 +0300

    Add files via upload

---
+++ PRO-C112-AUTOMATE-FILE-SEGREGATION-main/Project 111/Move_File.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+from_dir="C:\Users\hp\Downloads"
+to_dir="C:\Users\hp\medication safety final"
+
+list_of_files=os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name) 
+ 
+    if extension == '':
+        continue 
+    if extension in ['.txt','.pdf','.doc','.docx']:
+        path1=from_dir+'/'+file_name
+        path2=to_dir+'/'+"Document_Files"
+        path3=to_dir+'/'+"Document_Files"+'/'+file_name
+
+        if os.path.exist(path2):
+            print("Moving"+file_name+"...")
+            
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print("Moving"+file_name+"...")
+            shutil.move(path1,path3)
+
+
